75ec1552a47dcf3c187be0fb3d2467d49aece1da
oisheemaz <oishee.sde@gmail.com>
2021-09-15 13:17:02 +0530

    Added useContext() for auth-context

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,9 +1,12 @@
 import React from "react";
 import AuthContext from "../../Store/auth-context";
+import { useContext } from "react";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
+
   return (
     // <nav className={classes.nav}>
     //   <ul>
@@ -25,31 +28,51 @@ const Navigation = (props) => {
     //   </ul>
     // </nav>
 
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    // <AuthContext.Consumer>
+    //   {(ctx) => {
+    //     return (
+    //       <nav className={classes.nav}>
+    //         <ul>
+    //           {ctx.isLoggedIn && (
+    //             <li>
+    //               <a href="/">Users</a>
+    //             </li>
+    //           )}
+    //           {ctx.isLoggedIn && (
+    //             <li>
+    //               <a href="/">Admin</a>
+    //             </li>
+    //           )}
+    //           {ctx.isLoggedIn && (
+    //             <li>
+    //               <button onClick={props.onLogout}>Logout</button>
+    //             </li>
+    //           )}
+    //         </ul>
+    //       </nav>
+    //     );
+    //   }}
+    // </AuthContext.Consumer>
+
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
